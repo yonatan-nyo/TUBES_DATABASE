d2ea636f0607e369820fbf1f45c0a25b5cdf8c2a
Kurosue <adityarahmadeni30@gmail.com>
2025-05-27 19:55:25 +0700

    feat: Create view KontenPopuler Query

---
+++ backend/prisma/migrations/20250527094328_add_view_kontenpopuler/migration.sql
@@ -0,0 +1,27 @@
+-- Add this manually
+CREATE VIEW KreatorPopuler AS
+SELECT
+    k.nama AS nama_kreator,
+    k.bidang_kreasi,
+    pa.jumlah_pendukung_aktif AS jumlah_pendukung_aktif,
+    kom.jumlah_komentar AS jumlah_komentar,
+    pa.jumlah_pendukung_aktif * 3 + kom.jumlah_komentar AS skor_popularitas
+FROM Kreator k
+LEFT JOIN (
+    SELECT
+        l.id_kreator, 
+        COUNT(DISTINCT l.id_pendukung) AS jumlah_pendukung_aktif
+    FROM Langganan l
+    WHERE l.status = 'Aktif'
+    GROUP BY l.id_kreator
+) pa ON k.id_kreator = pa.id_kreator
+LEFT JOIN (
+    SELECT
+        ko.id_kreator,
+        COUNT(km.id_komentar) AS jumlah_komentar
+    FROM Komentar km
+    JOIN Konten ko ON km.id_konten = ko.id_konten
+    GROUP BY ko.id_kreator
+) kom ON k.id_kreator = kom.id_kreator 
+ORDER BY skor_popularitas DESC
+LIMIT 10;
