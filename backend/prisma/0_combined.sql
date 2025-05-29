b3638e6a13ce796d21c162261566271014a38acb
MaheswaraKaindra <19623197@std.stei.itb.ac.id>
2025-05-29 14:45:59 +0700

    feat: fitur tambahan 1

--- backend/prisma/0_combined.sql
+++ backend/prisma/0_combined.sql
@@ -100,3 +100,36 @@ SET
 END IF;
 
 END;
+
+-- Fitur Tambahan
+CREATE TRIGGER hitung_total_harga_insert
+AFTER INSERT ON pembelian
+FOR EACH ROW
+BEGIN
+  DECLARE v_harga DECIMAL(65,30);
+
+  SELECT harga INTO v_harga
+  FROM merchandise
+  WHERE id_merchandise = NEW.id_merchandise;
+
+  UPDATE pembelian
+  SET total_harga = v_harga * NEW.jumlah
+  WHERE id_merchandise = NEW.id_merchandise
+    AND id_pendukung = NEW.id_pendukung;
+END;
+
+CREATE TRIGGER hitung_total_harga_update
+AFTER UPDATE ON pembelian
+FOR EACH ROW
+BEGIN
+  DECLARE v_harga DECIMAL(65,30);
+
+  SELECT harga INTO v_harga
+  FROM merchandise
+  WHERE id_merchandise = NEW.id_merchandise;
+
+  UPDATE pembelian
+  SET total_harga = v_harga * NEW.jumlah
+  WHERE id_merchandise = NEW.id_merchandise
+    AND id_pendukung = NEW.id_pendukung;
+END;
--- backend/prisma/constraints/combine.ts
+++ backend/prisma/constraints/combine.ts
@@ -10,13 +10,15 @@ const typeSqlPath = join(__dirname, "type.sql");
 const databaseSqlPath = join(__dirname, "database.sql");
 const transitionSqlPath = join(__dirname, "transition.sql");
 const outputSqlPath = join(__dirname, "../0_combined.sql");
+const fiturTambahan1SqlPath = join (__dirname, "fitur_tambahan_1.sql");
 
 function combineSqlFiles() {
   const typeSql = readFileSync(typeSqlPath, "utf8");
   const databaseSql = readFileSync(databaseSqlPath, "utf8");
   const transitionSql = readFileSync(transitionSqlPath, "utf8");
+  const fiturTambahan1Sql = readFileSync(fiturTambahan1SqlPath, "utf8");
 
-  const combinedSql = `-- Type Constraints\n${typeSql}\n\n-- Database Constraints\n${databaseSql}\n\n-- Transition Constraints\n${transitionSql}`;
+  const combinedSql = `-- Type Constraints\n${typeSql}\n\n-- Database Constraints\n${databaseSql}\n\n-- Transition Constraints\n${transitionSql}\n\n-- Fitur Tambahan\n${fiturTambahan1Sql}`;
 
   writeFileSync(outputSqlPath, combinedSql, "utf8");
   console.log("SQL files combined successfully into combined.sql");
