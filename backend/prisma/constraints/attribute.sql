a23ac102a9b77f2b6ca8fda0f24a1b6f6d33bc60
yonatan-nyo <yonatan.njoto@gmail.com>
2025-05-30 14:59:21 +0700

    fixed enums to add alternatives

--- backend/prisma/constraints/attribute.sql
+++ backend/prisma/constraints/attribute.sql
@@ -1,4 +1,52 @@
--- 1. Atribut status harus bernilai terbatas (terdefinisi)
-ALTER TABLE `Langganan` MODIFY `status` ENUM ('Aktif', 'Pending', 'Expired') NOT NULL;
+/* 
+!!! 
+enums provided at schema.prisma thus doesnt need to copy this to migration file 
+[AUTOMATICALLY GENERATED]
+!!!
+*/
+--  on create
+CREATE TABLE `Konten` (
+    -- other attributes
+    `jenis` ENUM('Teks', 'Gambar', 'Audio', 'Video') NOT NULL,
+    -- other attributes & constraints
+);
+-- alter version assuming jenis exists in Konten table
+ALTER TABLE `Konten`
+MODIFY COLUMN `jenis` ENUM('Teks', 'Gambar', 'Audio', 'Video') NOT NULL;
 
-ALTER TABLE `OrderSpecialContent` MODIFY `status` ENUM ('Selesai', 'Diproses', 'Menunggu') NOT NULL;
+--  on create
+CREATE TABLE `Langganan` (
+    -- other attributes
+    `status` ENUM('Aktif', 'Pending', 'Expired') NOT NULL,
+    -- other attributes & constraints
+);
+-- alter version assuming status exists in Langganan table
+ALTER TABLE `Langganan`
+MODIFY COLUMN `status` ENUM('Aktif', 'Pending', 'Expired') NOT NULL;
+
+--  on create
+CREATE TABLE `OrderSpecialContent` (
+    -- other attributes
+    `status` ENUM('Selesai', 'Diproses', 'Menunggu') NOT NULL,
+    -- other attributes & constraints
+);
+-- alter version assuming status exists in OrderSpecialContent table
+ALTER TABLE `OrderSpecialContent`
+MODIFY COLUMN `status` ENUM('Selesai', 'Diproses', 'Menunggu') NOT NULL;
+
+--  on create
+CREATE TABLE `AktivitasPendukung` (
+    -- other attributes
+    `jenis_aktivitas` ENUM('Langganan', 'Komentar', 'Beli_Merchandise') NOT NULL,
+    -- other attributes & constraints
+);
+-- alter version assuming jenis_aktivitas exists in AktivitasPendukung table
+ALTER TABLE `AktivitasPendukung`
+MODIFY COLUMN `jenis_aktivitas` ENUM('Langganan', 'Komentar', 'Beli_Merchandise') NOT NULL;
+
+/* 
+!!! 
+enums provided at schema.prisma thus doesnt need to copy this to migration file 
+[AUTOMATICALLY GENERATED]
+!!!
+*/
